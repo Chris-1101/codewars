b12e5bd038be2dfbc0558cb283519daac45b001a
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-03 20:37:56 +0000

    Add header file

---
+++ 4kyu-mtpstr/include/mtpstr.hpp
@@ -0,0 +1,9 @@
+#ifndef MTPSTR_HPP
+#define MTPSTR_HPP
+
+std::string multiply(std::string num1, std::string num2);
+std::string add(std::string& num1, std::string& num2);
+std::string pad(const std::string& str, int amount);
+std::string trimLeading(const char& c, std::string& str);
+
+#endif
