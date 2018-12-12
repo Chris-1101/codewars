bec72e72172060e1a1fb758ca35610dc6503266c
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-12 19:18:40 +0000

    Create main.cpp

---
+++ 3kyu-last-digit-huge-power/src/main.cpp
@@ -0,0 +1,11 @@
+#include "lastdigit.hpp"
+#include "unittest.hpp"
+using namespace std;
+
+// Compiled using
+// g++ src/main.cpp src/findlast.cpp -o bin/main -Wall -iquote include/ -iquote ../unit-test/ -g
+
+int main(void)
+{
+  return 0;
+}
