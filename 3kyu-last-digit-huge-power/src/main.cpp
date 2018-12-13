8719536a113a66ecdccf0cf705fc114f40582641
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-13 16:50:28 +0000

    Implement some unit tests

--- 3kyu-last-digit-huge-power/src/main.cpp
+++ 3kyu-last-digit-huge-power/src/main.cpp
@@ -7,5 +7,19 @@ using namespace std;
 
 int main(void)
 {
+  // Unit Tests
+  unit_test<int, int>(last_digit({ 0,  0,  0}), 0);
+  unit_test<int, int>(last_digit({ 1,  2    }), 1);
+  unit_test<int, int>(last_digit({ 3,  4,  5}), 1);
+  unit_test<int, int>(last_digit({ 4,  3,  6}), 4);
+  unit_test<int, int>(last_digit({ 7,  6, 21}), 1);
+  unit_test<int, int>(last_digit({12, 30, 21}), 6);
+  unit_test<int, int>(last_digit({2, 2, 2, 0}), 4);
+
+  // Large Tests
+  unit_test<int, int>(last_digit({937640, 767456, 981242}), 0);
+  unit_test<int, int>(last_digit({123232, 694022, 140249}), 6);
+  unit_test<int, int>(last_digit({499942, 898102, 846073}), 6);
+
   return 0;
 }
