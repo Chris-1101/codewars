4c9676771f1bc2590b82f3d317c509f902b2c694
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-13 21:57:17 +0000

    :construction: Commit current algorithm, fails one test

--- 3kyu-last-digit-huge-power/src/lastdigit.cpp
+++ 3kyu-last-digit-huge-power/src/lastdigit.cpp
@@ -1,28 +1,33 @@
 #include <list>
 #include <cmath>
-#include <iostream>
 
 #include "lastdigit.hpp"
 
 int last_digit(std::list<int> arr)
 {
-  if (arr.size() == 0) return 1;
+  // Break conditions, extract last digit
   if (arr.size() == 1) return arr.back() % 10;
+  if (arr.size() == 0) return 1;
 
-  // Why are we using linked lists instead of vectors..?
+  // Extract the last 2 items in the list
   std::list<int> power(std::prev(arr.end(), 2), arr.end());
-  int base = power.front(), exponent = power.back(), next;
   arr.pop_back(); arr.pop_back();
 
-  if (exponent > 1) // replace with switch? + commit first
-  {
-    next = std::pow(base % 100, exponent % 4 + 4);
-  }
-  else
-  {
-    next = (exponent == 0) ? 1 : base;
-  }
+  // Treat them as the base and exponent for this recursion
+  long base = power.front(), exponent = power.back(), next;
+
+  // Calculate the current power
+  switch (exponent)                                                      // The last digit of a huge power will be the same as that of
+  {                                                                      // a reduced power consisting of the first digit of the base
+    case 0 : next = 1; break;                                            // (hence the mod 10) raised to the last two digits of the
+    case 1 : next = base % 100; break;                                   // exponent (hence the mod 100). Last digit in the result of a
+    default: next = (long)std::pow(base % 10, exponent % 4 + 4) % 100;   // power repeat in a pattern of 4 at most, so we can use mod 4
+  }                                                                      // to reduce the exponent. Offset by 4 for when remainder is 0
+  if (base != 0 && next == 0) next = 100;
+
+  // Add it as the last item in the list
   arr.push_back(next);
 
+  // Recursively deal with the next 2 items in the list
   return last_digit(arr);
 }
