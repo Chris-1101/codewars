fadfeef74f3b414eab28b9263e56c592be7beb0e
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-14 11:03:30 +0100

    :zap: Add basic skeleton

---
+++ 4kyu-roman-numerals-helper/src/romnum.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+#include "romnum.hpp"
+
+std::string RomanNumeralsHelper::to_roman(int n)
+{
+  return "";
+}
+
+int RomanNumeralsHelper::from_roman(std::string str)
+{
+  return 0;
+}
