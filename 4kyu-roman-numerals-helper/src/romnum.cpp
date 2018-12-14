bc5ceca57b59a5a843b41a78611232bf185ffa37
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-14 11:49:13 +0100

    :sparkles: Implement conversion from roman to arabic numerals

--- 4kyu-roman-numerals-helper/src/romnum.cpp
+++ 4kyu-roman-numerals-helper/src/romnum.cpp
@@ -1,4 +1,6 @@
+#include <map>
 #include <string>
+#include <algorithm>
 
 #include "romnum.hpp"
 
@@ -7,7 +9,26 @@ std::string RomanNumeralsHelper::to_roman(int n)
   return "";
 }
 
-int RomanNumeralsHelper::from_roman(std::string str)
+int RomanNumeralsHelper::from_roman(std::string roman)
 {
-  return 0;
+  std::map<char, int> values = {
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000}
+  };
+
+  int total = 0, previous_value = 0;
+  std::reverse(roman.begin(), roman.end());
+
+  for (unsigned i = 0; i < roman.length(); i++)
+  {
+    int value = values[roman[i]];
+    total += (value < previous_value) ? -value : value;
+    previous_value = value;
+  }
+  return total;
 }
