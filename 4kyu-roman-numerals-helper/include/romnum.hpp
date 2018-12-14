86303a7485bff30340c7b24abc78275b590d9d1c
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-14 10:47:51 +0100

    :zap: Add numerals class declaration

---
+++ 4kyu-roman-numerals-helper/include/romnum.hpp
@@ -0,0 +1,13 @@
+#ifndef ROMNUM_HPP
+#define ROMNUM_HPP
+
+#include <string>
+
+class RomanNumeralsHelper
+{
+  public:
+  std::string to_roman(int n);
+  int from_roman(std::string str);
+};
+
+#endif
