5e138aca86b991c62bd43b06f144340e3a54435b
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-21 09:10:34 +0100

    :recycle: Refactor multiplication algorithm

--- 4kyu-multiply-numbers-as-strings/src/mtpstr.cpp
+++ 4kyu-multiply-numbers-as-strings/src/mtpstr.cpp
@@ -1,82 +1,28 @@
-#include <iomanip>
-#include <sstream>
 #include <string>
 #include <vector>
-#include <algorithm>
 
 #include "mtpstr.hpp"
 
-// Multiply large numbers represented as strings
 std::string multiply(std::string num1, std::string num2)
 {
-  std::vector<std::string> results = {};
-  std::reverse(num1.begin(), num1.end());
-  std::reverse(num2.begin(), num2.end());
+  size_t len_num1 = num1.length(), len_num2 = num2.length();
+  size_t len_total = len_num1 + len_num2;
 
-  for (unsigned i = 0; i < num1.length(); ++i)
+  std::vector<int> tmp(len_total, 0);
+  std::string result(len_total, '0');
+
+  for (unsigned i = len_num1, max_i = len_num1 - 1; i--;)
   {
-    short carry = 0;
-    short n = num1[i] - '0';
-    std::stringstream res_total;
-    res_total << std::string(i, '0');
-
-    for (auto& m : num2)
+    for (unsigned j = len_num2, max_j = len_num2 - 1; j--;)
     {
-      short res_current = n * (m - '0') + carry;
-      res_total << res_current % 10;
-      carry = res_current / 10;
+      tmp[max_i - i + max_j - j] += (num1[i] - '0') * (num2[j] - '0');
     }
-    res_total << carry;
-
-    std::string result = res_total.str();
-    std::reverse(result.begin(), result.end());
-
-    results.push_back(result);
   }
-  std::string result = "0";
-
-  for (auto& num : results)
+  for(unsigned i = 0; i < len_total - 1; i++)
   {
-    result = add(num, result);
+    tmp[i + 1] += tmp[i] / 10;
+    result[len_total - 1 - i] = tmp[i] % 10 + '0';
+    result[0] = tmp[len_total - 1] % 10 + '0';
   }
-  return trimLeading('0', result);
-}
-
-// Add large numbers represented as strings
-std::string add(std::string& num1, std::string& num2)
-{
-  unsigned short max_len = std::max(num1.length(), num2.length());
-  num1 = pad(num1, max_len);
-  num2 = pad(num2, max_len);
-
-  std::reverse(num1.begin(), num1.end());
-  std::reverse(num2.begin(), num2.end());
-
-  std::stringstream res_total;
-  short carry = 0;
-
-  for (unsigned short i = 0; i < max_len; ++i)
-  {
-    short res_current = (num1[i] - '0') + (num2[i] - '0') + carry;
-    res_total << res_current % 10;
-    carry = res_current / 10;
-  }
-  std::string result = res_total.str();
-  std::reverse(result.begin(), result.end());
-
-  return result;
-}
-
-// Pad a number with leading zeroes
-std::string pad(const std::string& str, int amount)
-{
-  std::stringstream ss;
-  ss << std::setfill('0') << std::setw(amount) << str;
-  return ss.str();
-}
-
-// Remove leading zeroes, retain one for all-zeroes
-std::string trimLeading(const char& c, std::string& str)
-{
-  return str.erase(0, std::min(str.find_first_not_of(c), str.size() - 1));
+  return result.erase(0, std::min(result.find_first_not_of('0'), result.length() - 1));
 }
