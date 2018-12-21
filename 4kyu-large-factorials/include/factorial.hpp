2ce7baa926459dd440d2956a9783ef7f40158ed3
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-21 19:32:14 +0100

    :zap: Update header declarations

--- 4kyu-large-factorials/include/factorial.hpp
+++ 4kyu-large-factorials/include/factorial.hpp
@@ -1,12 +1,8 @@
-#ifndef FACTORIAL_HPP
-#define FACTORIAL_HPP
+#ifndef _FACTORIAL_HPP
+#define _FACTORIAL_HPP
 
 #include <string>
 
-std::string trimLeading(const char& c, std::string& str);
-std::string pad(const std::string& str, int amount);
-std::string add(std::string& num1, std::string& num2);
-std::string multiply(std::string num1, std::string num2);
 std::string factorial(int n);
 
 #endif
