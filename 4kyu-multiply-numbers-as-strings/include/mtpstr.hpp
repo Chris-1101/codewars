2c6cb129713585d7799925137caaffc743da63cd
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-21 09:08:47 +0100

    :zap: Update module header declarations

--- 4kyu-multiply-numbers-as-strings/include/mtpstr.hpp
+++ 4kyu-multiply-numbers-as-strings/include/mtpstr.hpp
@@ -1,9 +1,8 @@
-#ifndef MTPSTR_HPP
-#define MTPSTR_HPP
+#ifndef _MTPSTR_HPP
+#define _MTPSTR_HPP
+
+#include <string>
 
 std::string multiply(std::string num1, std::string num2);
-std::string add(std::string& num1, std::string& num2);
-std::string pad(const std::string& str, int amount);
-std::string trimLeading(const char& c, std::string& str);
 
 #endif
