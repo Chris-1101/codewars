672d7f39d886e50c06dbab2f569a78b41c0f616e
Chris-1101 <chrismb.noreply@gmail.com>
2018-12-25 08:55:24 +0100

    :art: Clean up code

--- 5kyu-directions-reduction/src/dirreduc.cpp
+++ 5kyu-directions-reduction/src/dirreduc.cpp
@@ -16,13 +16,9 @@ std::vector<std::string> DirReduction::dirReduc(std::vector<std::string> &arr)
     { "EAST" , {  1,  0 } }
   };
 
-  unsigned short x = 0;
-  unsigned short y = 1;
-
-  for (unsigned i = 1; i < res.size(); ++i)
+  for (unsigned i = 1, x = 0, y = 1; i < res.size(); ++i)
   {
-    std::string pos1 = res[i - 1];
-    std::string pos2 = res[i];
+    std::string pos1 = res[i - 1], pos2 = res[i];
 
     bool north_south = coords[pos1][x] == coords[pos2][x] && coords[pos1][y] != coords[pos2][y];
     bool west_east = coords[pos1][y] == coords[pos2][y] && coords[pos1][x] != coords[pos2][x];
@@ -34,37 +30,6 @@ std::vector<std::string> DirReduction::dirReduc(std::vector<std::string> &arr)
       i = 0;
     }
   }
-
-  // Reduce direction to sum of coordinates
-  // short sum_x = 0;
-  // short sum_y = 0;
-
-  // for (auto dir : arr)
-  // {
-  //   sum_x += coords[dir][0];
-  //   sum_y += coords[dir][1];
-  // }
-
-  // Extract reduced directions from sums
-  // NOTE: this assumes only one direction remains at the end
-  // As per test cases... If not, wrap each in for loop
-  // if (sum_y > 0)
-  // {
-  //   res.push_back("NORTH");
-  // }
-  // else if (sum_y < 0)
-  // {
-  //   res.push_back("SOUTH");
-  // }
-
-  // if (sum_x < 0)
-  // {
-  //   res.push_back("WEST");
-  // }
-  // else if (sum_x > 0)
-  // {
-  //   res.push_back("EAST");
-  // }
 
   return res;
 }
