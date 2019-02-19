d606980b1936a1e3bd22fdd5087f01ef2b824f00
Chris-1101 <chrismb.noreply@gmail.com>
2019-02-19 13:26:36 +0100

    :sparkles: Add C++ solution used for kata translation

---
+++ 5kyu-rot13-cipher/rot13.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+std::string rot13(std::string msg)
+{
+  std::transform(begin(msg), end(msg), begin(msg), [] (char c) -> char
+  {
+    if (!std::isalpha(c)) return c;
+
+    char offset = std::isupper(c) ? 'A' : 'a';
+    return (c - offset + 13) % 26 + offset;
+  });
+  return msg;
+}
